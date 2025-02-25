131353f2d1e8a8703a1da8dc6912418f6b2255c8
Sharren <sharrenepf@gmail.com>
2025-02-25 20:42:26 +0700

    tugas 1

---
+++ Dosen24.java
@@ -0,0 +1,13 @@
+public class Dosen24 {
+    public String kode;
+    public String nama;
+    public boolean jenisKelamin;
+    public int usia;
+
+    public Dosen24(String kode, String nama, boolean jenisKelamin, int usia) {
+        this.kode = kode;
+        this.nama = nama;
+        this.jenisKelamin = jenisKelamin;
+        this.usia = usia;
+    }
+}
