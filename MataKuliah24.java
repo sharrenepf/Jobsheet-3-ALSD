5a3a007cfb823c4105b71a408a9f999712325dbe
Sharren <sharrenepf@gmail.com>
2025-02-25 11:51:55 +0700

    percobaan 2

---
+++ MataKuliah24.java
@@ -0,0 +1,13 @@
+public class MataKuliah24 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public MataKuliah24(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+}
